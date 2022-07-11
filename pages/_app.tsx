d191d78cd4f01049ee48e3153a9cfe815053f22a
xelopsys <juraqulovjamshid7@gmail.com>
2022-07-11 16:53:02 +0500

    google tag is added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,28 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Script
+				strategy="lazyOnload"
+				src={`https://www.googletagmanager.com/gtag/js?id=UA-213502733-1`}
+			/>
+
+			<Script strategy="lazyOnload" id="">
+				{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-213502733-1', {
+    page_path: window.location.pathname,
+  });
+      `}
+			</Script>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
