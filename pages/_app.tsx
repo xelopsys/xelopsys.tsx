09aff6a8cb7752402afb16671caa4b9e3f16469f
xelopsys <juraqulovjamshid7@gmail.com>
2022-07-23 06:02:46 +0500

    google analytics in _app.tsx is updated and is rewritten

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,44 @@
+import { useEffect } from "react";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	useEffect(() => {
+		const handleRouteChange = (url: string) => {
+			gtag.pageview(url);
+		};
+		router.events.on("routeChangeComplete", handleRouteChange);
+		router.events.on("hashChangeComplete", handleRouteChange);
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+			router.events.off("hashChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<>
 			<Script
-				strategy="lazyOnload"
-				src={`https://www.googletagmanager.com/gtag/js?id=G-HNGT01NMJ2`}
+				strategy="afterInteractive"
+				src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
 			/>
-
-			<Script strategy="lazyOnload" id="">
-				{`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'G-HNGT01NMJ2', {
-    page_path: window.location.pathname,
-  });
-      `}
-			</Script>
+			<Script
+				id="gtag-init"
+				strategy="afterInteractive"
+				dangerouslySetInnerHTML={{
+					__html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+				}}
+			/>
 			<Component {...pageProps} />
 		</>
 	);
